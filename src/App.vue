71945bd42652c96c2278697a65b2e2cfe7ee4510
Luciel <jpmizidoro@gmail.com>
2024-07-19 13:57:44 -0300

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,69 @@
+<script>
+import Block from './components/BlockSelect.vue'
+import Result from './components/myResults.vue'
+
+export default {
+  name: 'App',
+  components: { Block, Result },
+  data() {
+    return {
+      isPlaying: false,
+      delay: null,
+      score: null
+    }
+  },
+  methods: {
+    start() {
+      this.delay = 2000 + Math.random() * 5000
+      this.isPlaying = true
+      this.score = null
+    },
+    endGame(reactionTime) {
+      this.score = reactionTime
+      this.isPlaying = false
+    },
+    showResults() {}
+  }
+}
+</script>
+
+<template>
+  <h1>Reaction Speed Timer</h1>
+  <div class="buttonDiv"><button @click="start" :disabled="isPlaying">play</button></div>
+  <Block v-if="isPlaying" :delay="delay" @end="endGame" />
+  <Result v-if="score != null" :score="score" />
+</template>
+
+<style scoped lang="scss">
+$--read-color: #797979;
+
+.buttonDiv {
+  margin: 1rem;
+}
+
+@mixin centerIt() {
+  display: flex;
+  justify-content: center;
+}
+
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #3b3b42;
+  margin-top: 60px;
+  @include centerIt;
+  align-items: center;
+
+  p {
+    @include centerIt;
+    color: $--read-color;
+  }
+}
+
+p {
+  display: flex;
+  justify-content: center;
+}
+</style>
---
+++ src/components/myResults.vue
@@ -0,0 +1,54 @@
+<template>
+  <div class="messageDiv">
+    <p>Reaction Time: {{ score }} ms</p>
+    <p class="message">{{ message }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ['score'],
+  data() {
+    return {
+      message: ''
+    }
+  },
+  methods: {
+    showResult() {
+      if (this.score < 350) {
+        this.message = 'Super-fast, great!.'
+      } else if (this.score < 500) {
+        this.message = "That's pretty good!"
+      } else {
+        this.message = 'Míssil R-36 enviado para sua localização.'
+      }
+    }
+  },
+  mounted() {
+    this.showResult()
+  }
+}
+</script>
+
+<style lang="scss">
+.messageDiv {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  margin-top: 2rem;
+
+  p {
+    display: inherit;
+    justify-content: inherit;
+    margin-top: 1rem;
+  }
+}
+
+.message {
+  color: #0faf87;
+  display: flex;
+  justify-content: center;
+  font-weight: bold;
+  font-size: 22px;
+}
+</style>
